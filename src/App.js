bdddaf31c6f6e4aa37d17936e590b007ab087de3
blase147 <solarmails2@gmail.com>
2023-05-25 15:50:25 +0100

    Add navigation homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, Routes, Route, Outlet,
+} from 'react-router-dom';
+import NavigationBar from './components/navigation/navigation';
 import './App.css';
+
+
+const NavigationBarLayout = () => (
+  <>
+    <NavigationBar />
+    <Outlet />
+  </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route element={<NavigationBarLayout />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+const App = () => (
+  <div className="App">
+    <Router>
+      <Routes>
+        <Route element={<NavigationBarLayout />} />
+      </Routes>
+    </Router>
+  </div>
+);
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './navigation.css';
+import {
+  FaTwitter, FaFacebookF, FaVine, FaPinterestP,
+} from 'react-icons/fa';
+import { TiSocialGooglePlus } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
+
+const NavigationBar = () => {
+  // const [style, setStyle] = useState('menu');
+  // const [menuStatus, setMenuStatus] = useState('open');
+
+  // const handleClick = () => {
+  //   if (menuStatus === 'open') {
+  //     setMenuStatus('close');
+  //     setStyle('menu active');
+  //   } else if (menuStatus === 'close') {
+  //     setMenuStatus('open');
+  //     setStyle('menu');
+  //   }
+  // };
+
+  // const navigate = useNavigate();
+
+  const navLinks = [
+    { url: '/', name: 'cars' },
+    { url: '/my_reservations', name: 'My Reservations' },
+    { url: '/add_car', name: 'Add Car' },
+    { url: '/delete_house', name: 'Delete Car' },
+  ].filter(Boolean);
+
+  return (
+    <div>
+      <div className="menu">
+        <div className="logo">Luxury Cars Lending</div>
+        <ul>
+          {navLinks.map(({ url, name }) => (
+            <li key={name}>
+              <Link to={url}>{name}</Link>
+            </li>
+          ))}
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+
+        </ul>
+
+        <div className="footer">
+          <div className="social">
+            <FaTwitter />
+            <FaFacebookF />
+            <TiSocialGooglePlus />
+            <FaVine />
+            <FaPinterestP />
+          </div>
+          <small>&copy; 2023 ENJOY YOUR RIDE</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
