7c4d1300d534d811bfae3d59619aa8c12cbb7455
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-06-01 02:54:20 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Cars from './components/cars/cars';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import DeleteCar from './components/deleteCars/DeleteCar';
+import AddCar from './components/addCars/addCar';
 
 const NavigationbarLayout = () => (
   <>
@@ -30,6 +31,7 @@ function App() {
               <Route path="/" element={<Cars />} />
               <Route path="/:id" element={<CarsDetails />} />
               <Route path="/delete_house" element={<DeleteCar />} />
+              <Route path="/add_car" element={<AddCar />} />
             </Route>
           </Routes>
         </Router>
