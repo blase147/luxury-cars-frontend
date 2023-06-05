865485edcd3dfd6a89819c5072cd48f7ecb7a841
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-06-05 16:31:30 +0200

    fixed the add car problem

--- src/components/addCars/addCar.js
+++ src/components/addCars/addCar.js
@@ -1,23 +1,31 @@
+/* eslint-disable no-console */
 import './addCars.scss';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import { IoCaretBackOutline } from 'react-icons/io5';
-import { createCar } from '../../Redux/Reducers/addCarSlice';
+import axios from 'axios';
 
 const AddCar = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const [isCarAdded, setIsCarAdded] = useState(false);
-
   const [formData, setFormData] = useState({
     name: '',
     brand: '',
     image: '',
     lending_fee: '',
     description: '',
+    car: '', // Added car property to formData
   });
-
+  const apiEndPoint = 'http://localhost:4000/api/v1/cars';
+  const addCar = async (carData) => {
+    try {
+      const response = await axios.post(apiEndPoint, { car: carData });
+      return response.data;
+    } catch (error) {
+      console.error('Error adding car:', error);
+      throw error;
+    }
+  };
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -25,20 +33,23 @@ const AddCar = () => {
       [name]: value,
     }));
   };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(createCar(formData));
-    setFormData({
-      name: '',
-      brand: '',
-      image: '',
-      lending_fee: '',
-      description: '',
-    });
-    setIsCarAdded(true);
+    try {
+      await addCar(formData); // Use the addCar function
+      setFormData({
+        name: '',
+        brand: '',
+        image: '',
+        lending_fee: '',
+        description: '',
+        car: '', // Reset car property
+      });
+      setIsCarAdded(true);
+    } catch (error) {
+      console.error('Error adding car:', error);
+    }
   };
-
   useEffect(() => {
     if (isCarAdded) {
       setTimeout(() => {
@@ -47,29 +58,28 @@ const AddCar = () => {
       }, 100);
     }
   }, [isCarAdded, navigate]);
-
   return (
     <>
       <div className="container">
         <form onSubmit={handleSubmit}>
           <h2>ADD LUXURY CARS</h2>
           <div className="inputEle first">
-            <input type="text" placeholder="Car Name" value={formData.name} onChange={handleChange} />
+            <input type="text" placeholder="Car Name" onChange={handleChange} name="name" />
           </div>
           <div className="inputEle">
-            <input type="text" placeholder="Car brand" value={formData.brand} onChange={handleChange} />
+            <input type="text" placeholder="Car brand" onChange={handleChange} name="brand" />
           </div>
           <div className="inputEle">
-            <input type="text" placeholder="Car ImageUrl" value={formData.image} onChange={handleChange} />
+            <input type="text" placeholder="Car ImageUrl" onChange={handleChange} name="image" />
           </div>
           <div className="inputEle">
-            <input type="number" placeholder="Car lending_fee" value={formData.lending_fee} onChange={handleChange} />
+            <input type="number" placeholder="Car lending_fee" onChange={handleChange} name="lending_fee" />
           </div>
           <div className="inputEle">
-            <textarea id="message" name="message" rows="4" placeholder="Car Description..." value={formData.description} onChange={handleChange} />
+            <textarea id="message" name="description" rows="4" placeholder="Car Description..." onChange={handleChange} />
           </div>
           <div className="inputEle submit">
-            <button type="button" className="btnsubmitf">
+            <button type="button" className="btnsubmitf" onClick={handleSubmit}>
               Add Car
             </button>
           </div>
@@ -77,7 +87,6 @@ const AddCar = () => {
             <button type="button" className="returnBtn" aria-label="Go back"><IoCaretBackOutline /></button>
           </Link>
         </form>
-
       </div>
     </>
   );
