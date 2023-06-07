f47d5d615eb9314c9bc715205bec1c3b1bc965c6
blase147 <solarmails2@gmail.com>
2023-06-07 14:35:03 +0100

    Update store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,6 +3,8 @@ import authReducer from './Reducers/authSlice';
 import carsReducer from './Reducers/carSlice';
 import carReducer from './Reducers/carDetailsSlice';
 import signUpReducer from './Reducers/regSlice';
+import reservationsReducer from './Reducers/reservationsSlice';
+import addCarReducer from './Reducers/addCarSlice';
 
 const store = configureStore({
   reducer: {
@@ -10,6 +12,8 @@ const store = configureStore({
     register: signUpReducer,
     cars: carsReducer,
     carDetail: carReducer,
+    reservations: reservationsReducer,
+    addCar: addCarReducer,
   },
 });
 
